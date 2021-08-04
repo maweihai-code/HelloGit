8da41d96cb73d79b127eede30ebcc9deeebbc8c0
maweihai <81176647+maweihai-code@users.noreply.github.com>
2021-08-04 22:06:29 +0800

    在线提交
    sss

--- HelloGit/src/main/java/com/itguigu/git/HelloGit.java
+++ HelloGit/src/main/java/com/itguigu/git/HelloGit.java
@@ -7,7 +7,8 @@ public class HelloGit {
         System.out.println("内测dev版本");
         System.out.println("内测beta版本");
         System.out.println("beta bug修复");
-
+        
         System.out.println("主干添加");
+        System.out.println("在线提交");
     }
 }
